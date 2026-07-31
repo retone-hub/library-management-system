8b63cbb56ed2ce72c8213f17976f6bf3977cba94
retone-hub <kevgib46@gmail.com>
2026-07-31 20:50:12 +0700

    upgrade optional book search using when()

--- backend/app/Http/Controllers/Api/BookController.php
+++ backend/app/Http/Controllers/Api/BookController.php
@@ -13,12 +13,23 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $search = $request->input('search'); //bisa diganti query tetapi hanya mengambil tipe data string, kalau input fleksibel
-
-        $books = Book::with([
+        $query = Book::query();
+        $query->with([
             'category',
             'author',
             'publisher',
-        ])->simplePaginate(10); // get bisa diganti paginate(10) untuk halamar 1,2,3,4,5,.. 100 or simplePaginate(10) cocok untuk previous dan next
+        ]);
+        $query->when($search, function ($query) use ($search) { // use itu membawa variabel dari luar function ke dalam closure (anonymous function)
+            $query->where('title', 'LIKE', "%{$search}%");
+        });
+
+        $books = $query->paginate(10);
+
+        // $books = Book::with([ // tidak digunakan karena sudah ada functionnya termasuk search
+        //     'category',
+        //     'author',
+        //     'publisher',
+        // ])->simplePaginate(10); // get bisa diganti paginate(10) untuk halamar 1,2,3,4,5,.. 100 or simplePaginate(10) cocok untuk previous dan next
 
         return BookResource::collection($books);
     }
